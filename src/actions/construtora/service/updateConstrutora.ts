baa92c5535ba8f32fc3959c7697ac640ad1b2d53
Vinicius Victor <vine.victordev@gmail.com>
2024-10-11 12:02:08 -0300

    update

--- src/actions/construtora/service/updateConstrutora.ts
+++ src/actions/construtora/service/updateConstrutora.ts
@@ -1,6 +1,7 @@
 'use server'
 import { PrismaClient } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { UpdateConstrutoraDto } from "../dto/updateConstrutora.dto";
 
 const prisma = new PrismaClient();
 
@@ -11,6 +12,13 @@ export default async function UpdateConstrutora(_: any, data: FormData) {
     const tel = data.get("telefoneSemMask") as string;
     const email = data.get("email") as string;
     const fantasia = data.get("fantasia") as string;
+
+    const dto = new UpdateConstrutoraDto(razaoSocial, tel, email, fantasia);
+    
+    const erroValidacao = dto.validate();
+    if(erroValidacao){
+        return { error: true, message: erroValidacao, data: null }
+    }
 
     const request = await prisma.nato_empresas.update({
         where: {
