39cdd9d0444c8078985d949604c76608daa5471f
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2024-10-21 16:50:44 -0300

    painel adm
    correção de bugs no  layout do painel ADM

--- src/components/relatorio_finaceiro/list_cobranca/index.tsx
+++ src/components/relatorio_finaceiro/list_cobranca/index.tsx
@@ -62,7 +62,7 @@ export default function ListCobranca() {
 
         `}
           </style>
-          <Box w={"full"} h={"25vh"} p={5} rounded="lg" boxShadow="2xl">
+          <Box w={"full"} minH={'fit-content'} minW={'fit-content'} p={5} rounded="lg" boxShadow={'2xl'}           >
             <Box w={"100%"} textAlign={"center"}>
               <Heading size="md">Lista de Cobranças em Aberto</Heading>
             </Box>
