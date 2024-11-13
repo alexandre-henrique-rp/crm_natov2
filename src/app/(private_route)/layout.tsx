5b206890dee932bf25fa327b6561b35c02d50c5c
Vinicius <vine.victordev@gmail.com>
2024-11-13 10:24:41 -0300

    correcao de bugs
    correcao bugs e toast

--- src/app/(private_route)/layout.tsx
+++ src/app/(private_route)/layout.tsx
@@ -1,27 +1,27 @@
 import BotaoJuncao from "@/components/botoes/bt_juncao";
 import FooterComponent from "@/components/footer";
-import { auth } from "@/lib/auth_confg";
+// import { auth } from "@/lib/auth_confg";
 import { Box, Flex } from "@chakra-ui/react";
-import { getServerSession } from "next-auth";
+// import { getServerSession } from "next-auth";
 
 interface PrivateLayoutProps {
   children: React.ReactNode;
 }
 
 export default  async function PrivateLayout({ children }: PrivateLayoutProps) {
- const session = await getServerSession(auth);
+//  const session = await getServerSession(auth);
 
- if (session) {
-   const request = await fetch("http://localhost:5050/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ token: session.token }),
-    });
-    const data = await request.json();
-    console.log("🚀 ~ data:", data);
-   };
+//  if (session) {
+//    const request = await fetch("http://localhost:5050/", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ token: session.token }),
+//     });
+//     const data = await request.json();
+//     console.log("🚀 ~ data:", data);
+//    };
   return (
     <Flex
       w={"100vw"}
