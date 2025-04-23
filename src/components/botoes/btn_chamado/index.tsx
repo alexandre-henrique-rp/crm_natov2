add2194ead2204d11260358510c044c6a1d60154
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-04-23 15:11:20 -0300

    create chamado

--- src/components/botoes/btn_chamado/index.tsx
+++ src/components/botoes/btn_chamado/index.tsx
@@ -39,7 +39,7 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
 
   const [files, setFiles] = useState<File[]>([]);
   const [imagePreviews, setImagePreviews] = useState<string[]>([]);
-  const [urlFinal, setUrlFinal] = useState<{ urlDownload: string, urlView: string }[]>([]);
+
   const [fileName, setFileName] = useState<string[]>([]);
 
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
@@ -55,71 +55,25 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
     handleFiles(droppedFiles);
   };
 
-  const handleFiles = async (uploadedFiles: FileList) => {
+  const handleFiles = (uploadedFiles: FileList) => {
     const filesArray = Array.from(uploadedFiles);
-    
-    for (const file of filesArray) {
-      if (
-          file.type === "application/pdf" ||
-          file.type === "image/jpeg" ||
-          file.type === "image/webp" ||
-          file.type === "image/png"
-      ) {
-          const formData = new FormData();
-          formData.append("file", file);
-          
-          try {
-              const response = await axios.post(`/api/chamado/post`, formData, {
-                  headers: {
-                      "Content-Type": "multipart/form-data",
-                  },
-              });
-              if (response.status === 200) {
-                  const url = response.data.data.url;
-                  const ulrView = response.data.data.viewUrl;
-                  const urlFileName = response.data.data.filename;
-                  const urlObj = { urlDownload: url, urlView: ulrView, urlFileName: urlFileName };
-                  
-                  const teste = fileName
-                  teste.push(urlFileName)
-                  setFileName(teste);
-                  setUrlFinal((prev) => [...prev, urlObj]);
-
-                  toast({
-                      title: "Arquivo salvo",
-                      status: "success",
-                      duration: 3000,
-                      isClosable: true,
-                  });
-              }
-          } catch (error) {
-              console.error(error);
-              toast({
-                  title: "Erro ao salvar arquivo",
-                  status: "error",
-                  duration: 3000,
-                  isClosable: true,
-              });
-          }
-      }
-    }
 
     const newImagePreviews = filesArray
-        .filter((file) => file.type.startsWith('image/'))
-        .map((file) => URL.createObjectURL(file));
+      .filter((file) => file.type.startsWith("image/"))
+      .map((file) => URL.createObjectURL(file));
 
     setFiles((prevFiles) => [...prevFiles, ...filesArray]);
     setImagePreviews((prevPreviews) => [...prevPreviews, ...newImagePreviews]);
   };
 
   const handleClick = () => {
-    document.getElementById('file-input')?.click();
+    document.getElementById("file-input")?.click();
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const uploadedFiles = event.target.files;
     if (uploadedFiles) {
-        handleFiles(uploadedFiles);
+      handleFiles(uploadedFiles);
     }
   };
 
@@ -127,47 +81,17 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
     event.stopPropagation();
     const name = fileName[index];
     const newFilesNames = fileName.filter((_, i) => i !== index);
-    setFileName(newFilesNames)
+    setFileName(newFilesNames);
     const newFiles = files.filter((_, i) => i !== index);
     const newImagePreviews = imagePreviews.filter((_, i) => i !== index);
     setFiles(newFiles);
     setImagePreviews(newImagePreviews);
-     handleDeleteImage(index ,name, event,)
   };
 
-  const handleDeleteImage = async (index: number, fileName: string ,event: React.MouseEvent) => {
-    event.stopPropagation();
-    const delImage = await axios(`/api/chamado/delete`,   {
-      method: 'DELETE',
-      data: {
-        image : fileName 
-      },
-    });
-    if (delImage.status === 200) {
-      const newUrlFinal = urlFinal.filter((_, i) => i !== index);
-      setUrlFinal(newUrlFinal);
-      toast({
-        title: "Sucesso",
-        description: "Imagem deletada com sucesso",
-        status: "success",
-        duration: 9000,
-        isClosable: true,
-      });
-    } else {
-      toast({
-        title: "Erro",
-        description: "Erro ao deletar imagem",
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-      });
-    }
-  }
-
   const handleDownload = (url: string) => {
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
-    link.download = '';
+    link.download = "";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -175,7 +99,7 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
 
   const Handle = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-  
+
     const userId = user?.id;
     if (!userId) {
       toast({
@@ -187,77 +111,82 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
       });
       return;
     }
-  
-    const data = {
-      solicitacao: id,       
-      descricao: descricao,   
-      status: 0,               
-      images: urlFinal,        
-      idUser: userId,          
-    };
-  
-    const response = await axios.post("/api/chamado/back/post", data);
-  
-    if (response.status === 200) {
-      setDescricao("");
-      setUrlFinal([]);
-      setFileName([]);
-      setFiles([]);
-      setImagePreviews([]);
-      onClose();
-  
-      toast({
-        title: "Sucesso",
-        description: "Chamado criado com sucesso!",
-        status: "success",
-        duration: 5000,
-        isClosable: true,
+
+    const formData = new FormData();
+    files.forEach((file) => {
+      formData.append("files", file);
+    });
+    formData.append("solicitacao", String(id));
+    formData.append("descricao", descricao);
+    formData.append("status", "0");
+
+    try {
+      const response = await fetch("/api/chamado/post", {
+        method: "POST",
+        body: formData,
       });
-    } else {
-      toast({
-        title: "Erro",
-        description: "Erro ao criar chamado.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+
+      const retorno = await response.json();
+
+      if (!response.ok) {
+        toast({
+          title: "Erro",
+          description: retorno.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        setDescricao("");
+        setFileName([]);
+        setFiles([]);
+        setImagePreviews([]);
+        onClose();
+
+        toast({
+          title: "Sucesso",
+          description: "Chamado criado com sucesso!",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
-  
-
 
   const handleModalClose = () => {
     setDescricao("");
     setImagePreviews([]);
     setFileName([]);
-    setUrlFinal([]);
     onClose();
   };
 
-
-
   return (
-    <><Tooltip label="Botão para solicitar suporte,solicitação ou alterações referente a este cliente." bg={"orange.700"}>
-      <Button
-        size={'sm'}
-        colorScheme="orange"
-        color={"white"}
-        cursor={"pointer"}
-        onClick={() => {
-          onOpen();
-        }}
-        textColor={'black'}
+    <>
+      <Tooltip
+        label="Botão para solicitar suporte,solicitação ou alterações referente a este cliente."
+        bg={"orange.700"}
       >
-        CRIAR CHAMADO
-      </Button>
-    </Tooltip>
+        <Button
+          size={"sm"}
+          colorScheme="orange"
+          color={"white"}
+          cursor={"pointer"}
+          onClick={() => {
+            onOpen();
+          }}
+          textColor={"black"}
+        >
+          CRIAR CHAMADO
+        </Button>
+      </Tooltip>
 
       <Modal isOpen={isOpen} size={"xl"} onClose={handleModalClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>
-            Abrir Chamado
-          </ModalHeader>
+          <ModalHeader>Abrir Chamado</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <Flex flexDir={"column"} gap={5}>
@@ -275,7 +204,7 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
                   value={descricao}
                 />
               </FormControl>
-              
+
               <Text fontSize={"1rem"}>Anexar Arquivo</Text>
               <Box textAlign="center">
                 <Box
@@ -287,7 +216,7 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
                   borderRadius="md"
                   p={5}
                   cursor="pointer"
-                  _hover={{ borderColor: 'gray.500' }}
+                  _hover={{ borderColor: "gray.500" }}
                 >
                   <Input
                     id="file-input"
@@ -296,12 +225,17 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
                     onChange={handleChange}
                     display="none"
                   />
-                  <Text fontSize={useBreakpointValue({ base: 'lg', md: 'xl' })}>
+                  <Text fontSize={useBreakpointValue({ base: "lg", md: "xl" })}>
                     Arraste arquivos aqui ou clique para selecionar
                   </Text>
                 </Box>
-                
-                <Box display="flex" flexWrap="wrap" justifyContent="center" mt={4}>
+
+                <Box
+                  display="flex"
+                  flexWrap="wrap"
+                  justifyContent="center"
+                  mt={4}
+                >
                   {imagePreviews.map((preview, index) => (
                     <Box key={index} position="relative" mr={2} mb={2}>
                       <Tooltip label="Excluir" placement="top" hasArrow>
@@ -324,7 +258,7 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
                         objectFit="cover"
                         borderRadius="md"
                         transition="transform 0.2s"
-                        _hover={{ transform: 'scale(1.2)' }}
+                        _hover={{ transform: "scale(1.2)" }}
                         onClick={() => handleDownload(preview)}
                       />
                     </Box>
@@ -335,14 +269,11 @@ export default function CreateChamado({ id }: CreateChamadoProps) {
           </ModalBody>
 
           <ModalFooter gap={3}>
-              <Button colorScheme="green" onClick={Handle}>
-                Confirmar
-              </Button>
-            
-            <Button
-              colorScheme="red"
-              onClick={handleModalClose}
-            >
+            <Button colorScheme="green" onClick={Handle}>
+              Confirmar
+            </Button>
+
+            <Button colorScheme="red" onClick={handleModalClose}>
               Cancelar
             </Button>
           </ModalFooter>
