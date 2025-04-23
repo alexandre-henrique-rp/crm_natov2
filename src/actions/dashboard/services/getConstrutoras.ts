53642f7d6f60faf67fa293ed45cac097c3578479
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-04-23 09:43:24 -0300

    fix

--- src/actions/dashboard/services/getConstrutoras.ts
+++ src/actions/dashboard/services/getConstrutoras.ts
@@ -1,26 +1,28 @@
-'use server'
+"use server";
 
-import { auth } from "@/lib/auth_confg";
-import { getServerSession } from "next-auth";
+import { GetSessionServer } from "@/lib/auth_confg";
 
-export default async function GetConstrutoras(){
-    const session = await getServerSession(auth);
-    
-    if(!session){
-        return null
+export default async function GetConstrutoras() {
+  const session = await GetSessionServer();
+
+  if (!session) {
+    return null;
+  }
+
+  const req = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/dashboard/construtoras`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session?.token}`,
+      },
     }
+  );
 
-    const req = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/dashboard/construtoras`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${session?.token}`
-        }
-    })
+  if (!req.ok) {
+    return null;
+  }
 
-    if(!req.ok){
-        return null
-    }
-
-    return await req.json()
+  return await req.json();
 }
