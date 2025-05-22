c68b62f50b22c3f069dbff5c1cecc6514466e50c
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-05-22 14:47:09 -0300

    Update route.ts

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -2,8 +2,7 @@ import { GetSessionClient } from "@/lib/auth_confg";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // const payload = await GetSessionServer();
-  const payload = await GetSessionClient();
+  const payload = await GetSessionClient()
   const session = payload ?? null;
   return NextResponse.json({ session });
 }
