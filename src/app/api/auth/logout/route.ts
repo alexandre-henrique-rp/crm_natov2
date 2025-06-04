89246b822ae2c1299f7dcb2da89817024ac6ba24
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-06-04 17:44:35 -0300

    update

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -2,12 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { DeleteSession } from "@/lib/auth_confg";
 
 export async function GET(request: NextRequest) {
-  try {
-    console.log("Logout");
     await DeleteSession();
     return NextResponse.redirect(new URL("/login", request.url));
-  } catch (error) {
-    console.log("Logout error", error);
-    return NextResponse.json({ message: "Erro ao realizar logout" }, { status: 500 });
-  }
 }
